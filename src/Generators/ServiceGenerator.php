ea99fb1569368631359e62e2304976f977bb7d40
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-05-16 00:32:27 +0000

    feat(service): conditionnal generation in event needed in service

--- src/Generators/ServiceGenerator.php
+++ src/Generators/ServiceGenerator.php
@@ -6,19 +6,18 @@ use Illuminate\Support\Facades\File;
 
 class ServiceGenerator
 {
-    public function generate($name, $withEvents = false)
+    public function generate(string $name, bool $withEvents = false): void
     {
-        $stub = File::get(__DIR__ . '/../Stubs/Service.stub');
+        $stubFile = $withEvents ? 'ServiceWithEvent.stub' : 'Service.stub';
+
+        $stub = File::get(__DIR__ . '/../Stubs/' . $stubFile);
 
         $stub = str_replace('{{class}}', $name, $stub);
         $stub = str_replace('{{model}}', $name, $stub);
 
         $path = app_path('Services/' . $name . 'Service.php');
 
-
         File::ensureDirectoryExists(dirname($path));
-
         File::put($path, $stub);
-        
     }
 }
